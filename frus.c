8a60d81b64919ffff040328c4209dc2fb377fa3a
AntimatterReactor <ezraalvarion71@gmail.com>
2022-02-03 17:40:48 +0700

    Refactor code to reduce size

--- frus.c
+++ frus.c
@@ -1,27 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int interpreter(char *rawIn, long inLen)
-{
-	long	lstack[600]	= {0};
-	char	pstack[30000]	= {0};
-	char	*stkit		= pstack;
-	char	skip		= 0;
-	long	lindex		= 0;
-	long	br_index	= 0;
-	long	i;
+#define THROW_IF(cond, msg) if (cond) { fputs(msg, stderr); return EXIT_FAILURE; }
+#define THROW_VAL_IF(cond, msg, val) if (cond) { fprintf(stderr, msg, val); return EXIT_FAILURE; }
 
-	for (i = 0; i < inLen; i++)
-	{
-		if (skip > 0)
-		{
-			if (rawIn[i] == ']') skip -= 1;
-			if (rawIn[i] == '[') skip += 1;
+static char pstack[30000] = {0}, *stkit = pstack, *buf = NULL, skip = 0;
+static long lstack[600] = {0}, lindex = 0, br_index = 0, i, len;
+static FILE *fp = NULL;
+
+void free_all(void) {
+	if (buf) free(buf);
+	if (fp) fclose(fp);
+}
+
+int main(int argc, char **argv) {
+	atexit(free_all);
+
+	THROW_IF(argc != 2, "Invalid number of arguments | Usage: frus <filename>\n");
+
+	fp = fopen(argv[1], "r");
+	THROW_VAL_IF(fp == NULL, "Err: Unable to open file '%s'\n", argv[1]);
+
+	THROW_IF(fseek(fp, 0L, SEEK_END) != 0, "Err: Unable to seek to end of file\n");
+
+	len = ftell(fp);
+	THROW_IF(len == -1L, "Err: Unable to tell the length of the file\n");
+
+	THROW_IF(fseek(fp, 0L, SEEK_SET) != 0, "Err: Unable to seek back to start of file\n");
+
+	buf = (char*) calloc(len, sizeof(char));
+	THROW_IF(buf == NULL, "Err: Not enough memory to allocate buffer\n");
+
+	fread(buf, sizeof(char), len, fp);
+	THROW_IF(ferror(fp), "Error: Unable read the file\n");
+
+	for (i = 0; i < len; i++) {
+		if (skip > 0) {
+			if (buf[i] == ']') skip -= 1;
+			else if (buf[i] == '[') skip += 1;
 			continue;
 		}
 
-		switch (rawIn[i])
-		{
+		switch (buf[i]) {
 			case '>': stkit++; break;
 			case '<': stkit--; break;
 
@@ -30,113 +50,23 @@ int interpreter(char *rawIn, long inLen)
 
 			case '[':
 				if	(*stkit != 0) lstack[++lindex] = i;
-				else if	(*stkit == 0)
-				{
-					skip += 1;
-					br_index = i;
-				}
+				else if	(*stkit == 0) skip += 1, br_index = i;
 				break;
 
 			case ']':
 				if (*stkit != 0) i = lstack[lindex--] - 1;
 				else lstack[lindex--] = 0;
-				if (lindex < 0)
-				{
-					fprintf(stderr, "Error: Unmatched ']' at index: %ld\n", i);
-					return 1;
-				}
+				THROW_VAL_IF(lindex < 0, "Err: Unmatched ']' at index: %ld\n", i);
 				break;
 
 			case '.': putchar(*stkit); break;
 			case ',': *stkit = getchar(); break;
 		}
 	}
-	if (skip == 1 || lindex > 0)
-	{
-		fprintf(stderr, "Error: Unmatched '[' at index: %ld\n", br_index);
-		return 1;
-	}
-	return 0;
-}
+	THROW_VAL_IF(skip == 1 || lindex > 0, "Err: Unmatched '[' at index: %ld\n", br_index);
 
-int main(int argc, char **argv)
-{
-	char *buffer;
-	FILE *fp;
-	long len;
-
-	if (argc < 2)
-	{
-		fprintf(stderr,
-		"Error: Not enough arguments\n| Usage: <filename>\n");
-		return EXIT_FAILURE;
-	}
-	else if (argc > 2)
-	{
-		fprintf(stderr,
-		"Error: Too much arguments\n| Usage: <filename>\n");
-		return EXIT_FAILURE;
-	}
-
-	fp = fopen(argv[1], "r");
-	if (fp == NULL)
-	{
-		fprintf(stderr,
-			"Error: Unable to open file\n"
-			"| Tip: Check if the file actually exists\n");
-		return EXIT_FAILURE;
-	}
-
-	if (fseek(fp, 0L, SEEK_END))
-	{
-		fprintf(stderr,
-			"Error: Unable to seek to the end of file\n"
-			"| No fix are available\n"
-			"| Please open an issue on github\n");
-		return EXIT_FAILURE;
-	}
-
-	len = ftell(fp);
-	if (len == -1L)
-	{
-		fprintf(stderr,
-			"Error: Unable to tell the length of the file\n"
-			"| No fix are available\n"
-			"| Please open an issue on github\n");
-		return EXIT_FAILURE;
-	}
-	if (fseek(fp, 0L, SEEK_SET))
-	{
-		fprintf(stderr,
-			"Error: Unable to seek back to the start of the file\n"
-			"| No fix are available\n"
-			"| Please open an issue on github\n");
-		return EXIT_FAILURE;
-	}
-
-	buffer = (char*) calloc(len, sizeof(char));
-	if (buffer == NULL)
-	{
-		fprintf(stderr,
-			"Error: Unable to allocate string buffer of size %ld\n"
-			"| Tip: Check if you have enough memory\n"
-			"| Please open an issue on github\n",
-			len);
-		return EXIT_FAILURE;
-	}
-
-	fread(buffer, sizeof(char), len, fp);
-	if (ferror(fp))
-	{
-		fprintf(stderr,
-			"Error: Unable read the file\n"
-			"| Tip: Check if the file is corrupted\n"
-			"| Please open an issue on github\n");
-		return EXIT_FAILURE;
-	}
-
-	if(interpreter(buffer, len)) return EXIT_FAILURE;
-
-	free(buffer);
 	return EXIT_SUCCESS;
 }
+
+#undef THROW_IF
+#undef THROW_VAL_IF
